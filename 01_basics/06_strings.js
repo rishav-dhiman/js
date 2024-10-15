6941198fe76e87ab32cfc37cffe32c0c7dbc04e9
Rishav Dhiman <160146114+rishav-dhiman@users.noreply.github.com>
2024-10-15 19:03:40 +0000

    string

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,14 @@
+const name = "rishav"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameNamee = new String('rishavxd')
+
+console.log(gameNamee[0]);
+console.log(gameNamee.__proto__.);
+
+
+console.log(gameNamee);
